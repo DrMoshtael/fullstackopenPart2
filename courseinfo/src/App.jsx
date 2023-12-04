cfba9c5216982cddee681add2febe8df397a7f82
Howard <howard@brightpage.tech>
2023-12-04 14:04:48 +0000

    Exercise 2.3 Courseinfo step8

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,19 +1,25 @@
 const Header = ({ courseName }) => <h1>{courseName}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <strong>Total of {sum} exercises</strong>
 
-const Part = ({ partName, partExercises }) => 
+const Part = ({ partName, partExercises }) =>
   <p>
     {partName} {partExercises}
   </p>
 
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part=>
-      <Part key={part.id} partName={part.name} partExercises={part.exercises} />)} 
-  </>   
+const Content = ({ parts }) => {
+  const sum = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises,0)
+  return (
+    <>
+      {parts.map(part =>
+        <Part key={part.id} partName={part.name} partExercises={part.exercises} />)}
 
-const Course = ({course}) => {
+      <Total sum={sum} />
+    </>
+  )
+}
+
+const Course = ({ course }) => {
 
   return (
     <>
@@ -42,6 +48,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
