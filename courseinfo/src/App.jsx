aebf1f6ff845fcc0738bb966ecc07cad7d71e025
Howard <howard@brightpage.tech>
2023-12-04 14:40:11 +0000

    Exercise 2.5 separate module

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,33 +1,4 @@
-const Header = ({ courseName }) => <h2>{courseName}</h2>
-
-const Total = ({ sum }) => <strong>Total of {sum} exercises</strong>
-
-const Part = ({ partName, partExercises }) =>
-  <p>
-    {partName} {partExercises}
-  </p>
-
-const Content = ({ parts }) => {
-  const sum = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
-  return (
-    <>
-      {parts.map(part =>
-        <Part key={part.id} partName={part.name} partExercises={part.exercises} />)}
-
-      <Total sum={sum} />
-    </>
-  )
-}
-
-const Course = ({ course }) => {
-
-  return (
-    <>
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
