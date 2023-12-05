4681aec38b57ef4362a546656a35b5efc08e1e56
Howard <howard@brightpage.tech>
2023-12-05 13:10:24 +0000

    Implemented without useEffect

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -10,21 +10,24 @@ const Person = ({ name, number }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  const initialPersons = [
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  ]
+  const [persons, setPersons] = useState(initialPersons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
-  const [filteredPersons, setFilteredPersons] = useState([])
+  const [filteredPersons, setFilteredPersons] = useState(initialPersons)
 
   const handleSubmission = (event) => {
     event.preventDefault()
     if (!persons.map(person => person.name.toLowerCase()).includes(newName.toLowerCase())) {
-      setPersons(persons.concat({ name: newName, number: newNumber }))
+      const updatedPersons = persons.concat({ name: newName, number: newNumber })
+      setPersons(updatedPersons)
+      setFilteredPersons(updatedPersons)
       setNewName('')
       setNewNumber('')
     }
@@ -43,14 +46,15 @@ const App = () => {
     console.log('before',newFilter)
     setNewFilter(event.target.value)
     console.log('after',newFilter)
-    
+    setFilteredPersons(persons.filter(person=>person.name.toLowerCase().includes(event.target.value.toLowerCase())))
+
   }
 
-  useEffect(() => {
-    console.log('before useEffect',newFilter)
-    setFilteredPersons(persons.filter(person=>person.name.toLowerCase().includes(newFilter.toLowerCase())))
-    console.log('after useEffect',newFilter)
-  }, [newFilter])
+  // useEffect(() => {
+  //   console.log('before useEffect',newFilter)
+  //   setFilteredPersons(persons.filter(person=>person.name.toLowerCase().includes(newFilter.toLowerCase())))
+  //   console.log('after useEffect',newFilter)
+  // }, [newFilter])
 
 
   return (
