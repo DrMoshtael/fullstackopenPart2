8ede74695796c1378444b5632ab3f74e1952f18b
Howard <howard@brightpage.tech>
2023-12-05 12:04:54 +0000

    Exercise 2.8 Phonebook step3

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,43 +1,68 @@
 import { useState } from 'react'
 
-const Person = ({name}) => <p>{name}</p>
+const Person = ({ name, number }) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{number}</td>
+    </tr>)
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    {
+      name: 'Arto Hellas',
+      number: '015-3098'
+    }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmission = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({name: newName  }))
-    setNewName('')
+    if (!persons.map(person => person.name.toLowerCase()).includes(newName.toLowerCase())) {
+      setPersons(persons.concat({ name: newName, number: newNumber }))
+      setNewName('')
+      setNewNumber('')
+    }
+    else { alert(`${newName} is already added to phonebook`) }
   }
-  
-  const handleInputChange = (event) => {
+
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
 
   return (
     <div>
-      <div>debug: {newName}</div>
+      <div>debug: {newNumber}</div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmission}>
         <div>
-          name: <input 
-                  value={newName} 
-                  onChange={handleInputChange}/>
+          name: <input
+            value={newName}
+            onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
-        {persons.map(person=>
-          <Person name={person.name} key={person.name} />)}
-      </div>
+      <table>
+        <tbody>
+          {persons.map(person =>
+            <Person name={person.name} number={person.number} key={person.name} />)}
+        </tbody>
+      </table>
     </div>
   )
 }
