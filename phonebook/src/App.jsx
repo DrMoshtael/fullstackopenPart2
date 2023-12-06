3b9f54d81eeb72703dfb81d9ed580265555e0252
Howard <howard@brightpage.tech>
2023-12-06 12:14:05 +0000

    Exercise 2.11 Phonebook step6

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ({ name, number }) => {
   return (
@@ -45,17 +46,23 @@ const PersonForm = ({ handleSubmission, newName, handleNameChange, newNumber, ha
 )
 
 const App = () => {
-  const initialPersons = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]
-  const [persons, setPersons] = useState(initialPersons)
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
-  const [filteredPersons, setFilteredPersons] = useState(initialPersons)
+  const [filteredPersons, setFilteredPersons] = useState([])
+
+  useEffect(()=> {
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response=>{
+      console.log('promise fulfilled')
+      setPersons(response.data)
+      setFilteredPersons(response.data)
+    })
+  },[])
 
   const handleSubmission = (event) => {
     event.preventDefault()
